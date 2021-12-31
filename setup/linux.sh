0407ffb258e7a865010f2be7825203b4769c39db
Troy Dai <git@troydai.com>
2021-12-31 12:27:45 -0800

    Use https instead git schema for ohmyzsh submodule

--- setup/linux.sh
+++ setup/linux.sh
@@ -8,11 +8,11 @@ DOTFILE_ROOT=$(cd $(dirname $0); cd ..; pwd)
 mkdir -p ~/.config/nvim
 mkdir -p ~/.local/share/nvim/site/autoload/
 
-ln -s $DOTFILE_ROOT/.zshrc					~/.zshrc
-ln -s $DOTFILE_ROOT/tmux/tmux.conf			~/.tmux.conf
-ln -s $DOTFILE_ROOT/nvim/init.vim			~/.config/nvim/init.vim	 
-ln -s $DOTFILE_ROOT/nvim/vim-plug/plug.vim  ~/.local/share/nvim/site/autoload/plug.vim	 
-ln -s $DOTFILE_ROOT/fzf						~/.fzf
+ln -s $DOTFILE_ROOT/.zshrc                 ~/.zshrc
+ln -s $DOTFILE_ROOT/tmux/tmux.conf         ~/.tmux.conf
+ln -s $DOTFILE_ROOT/nvim/init.vim          ~/.config/nvim/init.vim	 
+ln -s $DOTFILE_ROOT/nvim/vim-plug/plug.vim ~/.local/share/nvim/site/autoload/plug.vim	 
+ln -s $DOTFILE_ROOT/fzf                    ~/.fzf
 
 nvim +PlugInstall +qa
 ~/.fzf/install
