93b714c4e5898cc4dd86611ab8d114d62faec02c
Troy Dai <git@troydai.com>
2021-12-31 18:24:43 -0800

    init remote script

---
+++ remote-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Remote install dotfile / git@troydai.com"
+
+DOT_NAME=dotfiles
+DOT_DIR=$HOME/$DOT_NAME
+
+git clone https://github.com/troydai/dotfiles.git $DOT_DIR --recursive
+
+$DOT_DIR/setup/linux.sh
