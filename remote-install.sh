65934d7fc7f427f7e650a5c4b1b60d562443272d
Troy Dai <git@troydai.com>
2022-01-02 09:15:09 -0800

    rename dotfiles folder name

--- remote-install.sh
+++ remote-install.sh
@@ -2,7 +2,7 @@
 
 echo "Remote install dotfile / git@troydai.com"
 
-DOT_NAME=dotfiles
+DOT_NAME=.dotfiles
 DOT_DIR=$HOME/$DOT_NAME
 
 git clone https://github.com/troydai/dotfiles.git $DOT_DIR --recursive
