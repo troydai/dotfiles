b273d31e098935d0110b57a463125237292d779e
Troy Dai <git@troydai.com>
2022-08-12 22:58:50 -0700

    Add mac.sh

---
+++ setup/mac.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+brew update
+brew install -y neovim tmux zsh
+
+DOTFILE_ROOT=$(cd $(dirname $0); cd ..; pwd)
+
+mkdir -p ~/.config/nvim
+mkdir -p ~/.local/share/nvim/site/autoload/
+
+ln -s $DOTFILE_ROOT/.zshrc                 ~/.zshrc
+ln -s $DOTFILE_ROOT/tmux/tmux.conf         ~/.tmux.conf
+ln -s $DOTFILE_ROOT/nvim/init.vim          ~/.config/nvim/init.vim	 
+ln -s $DOTFILE_ROOT/nvim/vim-plug/plug.vim ~/.local/share/nvim/site/autoload/plug.vim	 
+ln -s $DOTFILE_ROOT/fzf                    ~/.fzf
+
+nvim +PlugInstall +qa
+~/.fzf/install --no-fish --no-update-rc --completion --key-bindings
+
